bb322cf6874017ce6cb3a6231bf97d99e89abf4c
luisferliza <luisferliza@gmail.com>
2019-07-21 18:40:08 -0600

    Primera Version

---
+++ CalculadoraVB/CalculadoraVB/Module1.vb
@@ -0,0 +1,22 @@
+﻿Imports System.IO
+
+Module Module1
+
+    Sub Main()
+        MyParser.Setup()
+
+        Dim fileReader As String
+        fileReader = My.Computer.FileSystem.ReadAllText(Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "entrada.txt"))
+
+        If MyParser.Parse(New StringReader(fileReader)) Then
+            Console.WriteLine()
+            Console.WriteLine("Ejecutado Exitosamente")
+        Else
+            Console.WriteLine("No se pudo ejecutar")
+        End If
+
+        Console.ReadLine()
+
+    End Sub
+
+End Module
